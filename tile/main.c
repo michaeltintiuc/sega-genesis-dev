0674bb73e7bf085f8bea19254b1f882000cf7f50
Michael Tintiuc <contact@michaeltintiuc.com>
2018-03-31 18:16:26 +0300

    Fix cast error

--- tile/main.c
+++ tile/main.c
@@ -19,7 +19,7 @@ int main() {
     // w/8 = width in tiles of the bitamp
     // the 3rd arg is needed because you could load only a part of the bitmap
     // if you want but SGDK needs the width as reference
-    VDP_loadBMPTileData(moon.image, 1, w / 8, h / 8, w / 8);
+    VDP_loadBMPTileData((const u32 *)moon.image, 1, w / 8, h / 8, w / 8);
 
     // draw the moon at (12,12)
     VDP_fillTileMapRectInc(PLAN_B, TILE_ATTR_FULL(PAL1, 0, 0, 0, TILE1), 12, 12, w / 8, h / 8);
